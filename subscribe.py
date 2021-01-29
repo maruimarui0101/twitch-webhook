2bce59e5cf2bbd7941e332cb1f1ee994092eb4be
maruimarui0101 <cd.manalansan@gmail.com>
2021-01-29 02:58:28 +0000

    Initial commit

---
+++ subscribe.py
@@ -0,0 +1,65 @@
+import requests
+import os 
+from dotenv import load_dotenv
+
+import json
+
+load_dotenv()
+
+AUTH_ENDPOINT ="https://id.twitch.tv/oauth2/token"
+SUB_ENDPOINT = 'https://api.twitch.tv/helix/eventsub/subscriptions'
+
+def authenticate():
+
+    AutParams = {'client_id': f"{os.getenv('TWITCH_CLIENT_ID')}"
+    , 'client_secret': f"{os.getenv('TWITCH_SECRET')}"
+    , 'grant_type': 'client_credentials'}
+
+    print("REQUESTING ACCESS TOKEN")
+
+    AutCall = requests.post(url=AUTH_ENDPOINT, params=AutParams)
+
+    auth_resp = AutCall.json()
+    access_token = auth_resp['access_token']
+
+    print(access_token)
+
+    return access_token
+
+def make_sub(access_token):
+
+    payload = {
+        "type": "channel.follow",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": "12826"
+        },
+        "transport": {
+            "method": "webhook",
+            "callback": f"{os.getenv('TUNNEL_URL')}",
+            "secret": "s3cRe7"
+        }
+    }
+
+    headers = {"Client-ID": f"{os.getenv('TWITCH_CLIENT_ID')}",
+    "Authorization": f"Bearer {access_token}"}
+
+    r = requests.post(SUB_ENDPOINT,headers=headers, data= payload)
+
+    print(r.json())
+
+    return r.json()
+
+def get_subs(access_token):
+
+    headers = {"Client-ID": f"{os.getenv('TWITCH_CLIENT_ID')}",
+    "Authorization": f"Bearer {access_token}"}
+
+    r = requests.get(SUB_ENDPOINT,headers=headers)
+
+    return r.json()
+
+
+if __name__ == "__main__":
+    access_token = authenticate()
+    make_sub(access_token)
